3ad7a5e777cdb1270494942c447adbd2d1ddf8e9
Kirtiman29 <kirtimangupta29@gmail.com>
2025-06-06 22:58:27 +0530

    Initial commit done

---
+++ app/src/main/java/com/example/myshoppingadminapp/domain/model/ProductDataModel.kt
@@ -0,0 +1,14 @@
+package com.example.myshoppingadminapp.domain.model
+
+data class ProductDataModel (
+    val name: String = "",
+    val description : String = "",
+    val price: String = "",
+    val finalprice: String = "",
+    val category: String = "",
+    val imageUri : String = "",
+    val date: Long = System.currentTimeMillis(),
+    val availableUnits: Int = 0,
+    val isAvailable: Boolean = true,
+    val productId: String = ""
+)
